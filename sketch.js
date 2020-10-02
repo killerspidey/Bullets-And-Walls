ce7afabe697dd0d325c235202b050cfec40ac1aa
killerspidey <71497149+killerspidey@users.noreply.github.com>
2020-10-02 12:34:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var bullet, wall;
+var speed, weight, thickness;
+
+function setup() {
+  createCanvas(1600, 400);
+  createSprite(400, 200, 50, 20);
+  speed = random(223, 331);
+  thickness = random(22, 83);
+  weight = random(30, 52);
+  bullet = createSprite(50, 200, 50, 20);
+  wall = createSprite(1200, 200, thickness, height / 2);
+  bullet.velocityX = speed;
+}
+
+function draw() {
+  background("black");
+  bullet.shapeColor = color("white");
+
+  if (hasCollided(bullet, wall)) {
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed / (thickness * thickness * thickness);
+    if (damage > 10) {
+      wall.shapeColor = color(255, 0, 0);
+    }
+    if (damage < 10) {
+      wall.shapeColor = color(0, 255, 0);
+    }
+  }
+  drawSprites();
+}
+
+function hasCollided(bullet, wall) {
+  bulletRightEdge = bullet.x + bullet.width;
+  wallLeftEdge = wall.x;
+  if (bulletRightEdge >= wallLeftEdge) {
+    return true;
+  }
+  return false;
+}
